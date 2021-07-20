8ecd84ac750230535bc6ba31bdbd47dd3b4e1d3a
sdemarch <stefano.demarchi@edu.unige.it>
2021-07-20 10:08:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="NeVer2",
-    version="0.1.1.dev2",
+    version="1.0-alpha",
     author="Dario Guidotti, Stefano Demarchi",
     author_email="{dario.guidotti|stefano.demarchi}@edu.unige.it",
     license='GNU General Public License with Commons Clause License Condition v1.0',
-    description="Tool for the creation and the conversion of Neural Network models.",
+    description="Tool for the creation, conversion, training and verification of Neural Network models.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/NeVerTools/NeVer2",
@@ -21,6 +21,5 @@ setuptools.setup(
         "Operating System:: OS Independent",
     ],
     python_requires='>=3.8',
-    install_requires=['numpy', 'PyQt5', 'pynever', 'onnx', 'torch', 'tensorflow', 'pysmt'],
+    install_requires=['pynever', 'PyQt5', 'numpy', 'onnx', 'torch', 'torchvision', 'pysmt'],
 )
-
